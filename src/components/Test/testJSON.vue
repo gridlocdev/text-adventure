0b3c50ba2d80e3b3ca42b73840abf7696fdec511
gridlocdev <60323737+gridlocdev@users.noreply.github.com>
2021-01-07 16:48:58 -0700

    Updated test JSON data's chapter 2

--- src/components/Test/testJSON.vue
+++ src/components/Test/testJSON.vue
@@ -51,8 +51,8 @@
       <h1>Ending data:</h1>
       <v-spacer></v-spacer>
       <span>
-        {{ storyJSON.Chapters[0].ChapterSections[3].SectionData.title }}
-        {{ storyJSON.Chapters[0].ChapterSections[3].SectionData.subText }}
+        {{ storyJSON.Chapters[0].ChapterSections[5].SectionData.title }}
+        {{ storyJSON.Chapters[0].ChapterSections[5].SectionData.subText }}
       </span>
     </div>
   </v-container>
@@ -105,25 +105,6 @@ export default {
                 },
               },
               {
-                SectionType: "Ending",
-                SectionData: {
-                  title: "Chapter 1 Complete!",
-                  subText: "You've successfully started your journey!",
-                },
-              },
-            ],
-          },
-          {
-            ChapterName: "Chapter 2",
-            ChapterSections: [
-              {
-                SectionType: "Intro",
-                SectionData: {
-                  title: "Chapter 2",
-                  subText: "The Unruly King",
-                },
-              },
-              {
                 SectionType: "TextSection",
                 SectionData: [
                   {
@@ -147,8 +128,8 @@ export default {
                   },
                   choicesMetadata: {
                     correctChoice: "1",
-                    gameOverText: "You cut his hand. GAMEOVER",
-                    successText: "He hands you the sword, good job!",
+                    gameOverText: "GAMEOVER: 2",
+                    successText: "SUCCESS: 2",
                   },
                 },
               },
@@ -157,6 +138,89 @@ export default {
                 SectionData: {
                   title: "Chapter 1 Complete!",
                   subText: "You've successfully started your journey!",
+                },
+              },
+            ],
+          },
+          {
+            ChapterName: "Chapter 2",
+            ChapterSections: [
+              {
+                SectionType: "Intro",
+                SectionData: {
+                  title: "Chapter 2",
+                  subText: "The Other Guy",
+                },
+              },
+              {
+                SectionType: "TextSection",
+                SectionData: [
+                  {
+                    text: "A guy's there.",
+                  },
+                  {
+                    text: "This other guy is pretty cool.",
+                  },
+                  {
+                    text: "What's his name again? Who knows.",
+                  },
+                ],
+              },
+              {
+                SectionType: "ChoiceSection",
+                SectionData: {
+                  text: "Should you ask his name?",
+                  choices: {
+                    choice1: "Heck yeah",
+                    choice2: "Nah, im good",
+                  },
+                  choicesMetadata: {
+                    correctChoice: "1",
+                    gameOverText: "He roundhouse kicks u into the atmosphere.",
+                    successText:
+                      "My name is... Chuck norris. Here's 10 million dollars.",
+                  },
+                },
+              },
+              {
+                SectionType: "TextSection",
+                SectionData: [
+                  {
+                    text:
+                      "Now equipped with 10 million dollars, you set your sights on the moon.",
+                  },
+                  {
+                    text:
+                      "You call NASA, but they aren't too interested in your offer.",
+                  },
+                  {
+                    text:
+                      "10 million is a lot, but you might not be able to go to the moon with that.",
+                  },
+                ],
+              },
+              {
+                SectionType: "ChoiceSection",
+                SectionData: {
+                  text: "You need more money, what should you do?",
+                  choices: {
+                    choice1: "Become a hotdog stand guy",
+                    choice2: "Go begging in old school runescape",
+                  },
+                  choicesMetadata: {
+                    correctChoice: "2",
+                    gameOverText:
+                      "You end up liking hot dogs so much that u explode",
+                    successText:
+                      "You find a runescape player that's sick nasty and gives you 400 billion GP",
+                  },
+                },
+              },
+              {
+                SectionType: "Ending",
+                SectionData: {
+                  title: "Chapter 2 Complete!",
+                  subText: "You're rich! Muahahahahaaaaa",
                 },
               },
             ],
